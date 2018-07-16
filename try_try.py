56b6769ca7c02c187b7c7d72ef149a17718e0162
Arttu Ylhävuori <arjymoi@hotmail.com>
2018-07-16 18:08:43 +0300

    luvun 10 kokoava esimerkki

--- try_try.py
+++ try_try.py
@@ -4,11 +4,21 @@
 import sys
 
 try:
-    tiedosto_kahva = open("tiedosto.txt","r",encoding="utf-8")
+    tiedosto_kahva = open("tiedosto.txt", "r", encoding="utf-8")
     try:
         rivi = tiedosto_kahva.readline()
         if len(rivi) == 0:
             print("Tiedosto on tyhjä!")
         else:
-            rivi_muuttuja = rivi[:-1] # Määritetään asetus_vipu
-    
+            rivi_muuttuja = rivi[:-1]  # Määritetään asetus_vipu
+    except IOError:
+        print("Tiedostosta lukeminen epäonnistui.")
+        sys.exit(-2)
+    finally:
+        tiedosto_kahva.close()  # Suljetaan onnistuneesti avattu tiedosto
+except IOError:
+    print("Tiedoston avaaminen epäonnistui.")
+    sys.exit(-1)
+
+print("Tiedosto-operaatiot suoritettu onnistuneesti.")
+print("Kiitos ohjelman käytöstä.")
